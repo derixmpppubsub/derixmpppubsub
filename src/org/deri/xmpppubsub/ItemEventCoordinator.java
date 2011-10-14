0b7b41b27b514d6aa0ee8dc98f45302719eabf85
janaya <julia.anaya@gmail.com>
2011-10-14 13:16:06 +0100

    Cleaning, removing unneeded logs

--- src/org/deri/xmpppubsub/ItemEventCoordinator.java
+++ src/org/deri/xmpppubsub/ItemEventCoordinator.java
@@ -15,25 +15,36 @@ import org.xml.sax.SAXException;
  * @author maciejdabrowski
  * @author Julia Anaya
  */
+
 public class ItemEventCoordinator implements ItemEventListener {
     static Logger logger = Logger.getLogger(ItemEventCoordinator.class);
     String fileName;
     String endpoint;
     String subSeq;
-    
+
+    /**
+     *
+     * @param subSeq
+     * @param fileName
+     * @param endpoint
+     */
     public ItemEventCoordinator(String subSeq, String fileName, String endpoint) {
 //        logger.debug("new itemeventcoordinator");
         this.fileName = fileName;
         this.endpoint = endpoint;
         this.subSeq = subSeq;
     }
-    
+
+    /**
+     *
+     * @param items
+     */
     @Override
     public void handlePublishedItems(ItemPublishEvent items) {
         long end = System.currentTimeMillis();
 //        long end = System.nanoTime();
 //        logger.debug("en listener");
-        
+
         try {
             SPARQLQuery sq = new SPARQLQuery();
 //            SPARQLWrapper sw = new SPARQLWrapper();
@@ -41,42 +52,40 @@ public class ItemEventCoordinator implements ItemEventListener {
             String[] columns = new String[4];
             String itemId,pubSeq, nTriples, pTime,  start, query, result, msgSize, line;
             Long insertTime, msgTime, totalTime;
-                    
+
             List<Item> its = items.getItems();
             for(Item item : its) {
                 itemId = item.getId();
                 logger.debug("received item id" + itemId);
-                
+
                 // msgId format:
-//                String msgId = "pub" + i + "of" + numberOfPublishers 
+//                String msgId = "pub" + i + "of" + numberOfPublishers
 //                + ",triples" + numberOfTriples + ",ctime" + time.toString();
-//                String msgId = "pub" + i + "of" + numberOfPublishers 
-//                + "," + numberOfTriples + "," + time.toString();
-        
+
                 columns = itemId.split(",");
                 pubSeq = columns[0];
                 nTriples = columns[1];
                 pTime = columns[2];
                 start = columns[3];
-                
+
                 msgTime = end - Long.valueOf(start);
                 //logger.debug("elapsed time: " + msgTime);
 
                 query = sq.fromXML(item.toXML());
-                //logger.debug("query: " + query);                
+                //logger.debug("query: " + query);
 //                result = sw.runQuery(query, endpoint, true);
                 Object[] ret = SPARQLWrapper.runQuery(query, endpoint, true);
                 insertTime = (Long)ret[1];
 //                result = (String)ret[0];
-                
+
                 totalTime = Long.valueOf(pTime) + msgTime + insertTime;
                 //logger.debug("total time:" + totalTime);
-                
+
                 msgSize = Integer.toString(item.toString().length());
                 // file header format:
                 // "subscriber seq, publisher seq,triples/msg,msg size(chars),publisher store time (ms),publish time (ms),subscriber store time (ms),total time (msg)"
-                line = subSeq + "," + pubSeq + "," + nTriples + "," + msgSize 
-                      + "," + pTime + "," + msgTime.toString() + "," 
+                line = subSeq + "," + pubSeq + "," + nTriples + "," + msgSize.toString()
+                      + "," + pTime + "," + msgTime.toString() + ","
                       + insertTime.toString() + "," + totalTime.toString() + "\n";
                 writer.write(line);
                 writer.flush();
@@ -95,6 +104,6 @@ public class ItemEventCoordinator implements ItemEventListener {
         } catch (Exception e) {
             logger.error(e);
         }
-        
+
     }
 }
